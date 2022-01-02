9a227e41d8708c4bb8de67d75576711371e8b71b
Stefan Niederhauser <ghuder5@gmx.ch>
2022-01-02 01:16:56 +0100

    more sophisticated verb model

--- src/main/kotlin/guru/nidi/dicamo/Grammar.kt
+++ src/main/kotlin/guru/nidi/dicamo/Grammar.kt
@@ -1,70 +1,93 @@
 package guru.nidi.dicamo
 
+import guru.nidi.dicamo.Form.*
 import org.slf4j.LoggerFactory
 import java.text.Normalizer
+import kotlin.math.max
+import kotlin.math.min
+
+//for verb conjugations, see
+//https://ca.wiktionary.org/wiki/Viccionari:Conjugaci%C3%B3_en_catal%C3%A0/paradigmes
 
 val log = LoggerFactory.getLogger("grammar")
 
 private val VERB_TYPES = listOf("ar", "er", "re", "dre", "ndre", "ur", "ure")
 
+private enum class Form { GERUNDI, PARTICIPI, PRESENT, IMPERFET, SIMPLE, FUTUR, SUB_PRESENT, SUB_IMPERFET, CONDICIONAL, IMPERATIU }
+
 private val VERB_ENDINGS = mapOf(
-    listOf("ar") to setOf(
-        "ant",
-        "at", "ada", "ats", "ades",
-        "o", "es", "a", "em", "eu", "en",
-        "ava", "aves", "ava", "avem", "aveu", "aven",
-        "i", "ares", "a", "arem", "areu", "aren",
-        "are", "aras", "ara", "arem", "areu", "aran",
-        "i", "is", "i", "em", "eu", "in",
-        "es", "essis", "es", "essim", "essiu", "essin",
-        "aria", "aries", "aria", "ariem", "arieu", "arien",
-        "a", "i", "em", "eu", "in"
+    listOf("ar") to mapOf(
+        "" to mapOf(
+            GERUNDI to listOf("ant"),
+            PARTICIPI to listOf("at", "ada", "ats", "ades"),
+            PRESENT to listOf("o", "es", "a", "em", "eu", "en"),
+            IMPERFET to listOf("ava", "aves", "ava", "àvem", "àveu", "aven"),
+            SIMPLE to listOf("í", "ares", "à", "àrem", "àreu", "aren"),
+            FUTUR to listOf("aré", "aràs", "arà", "arem", "areu", "aran"),
+            SUB_PRESENT to listOf("i", "is", "i", "em", "eu", "in"),
+            SUB_IMPERFET to listOf("és", "essis", "és", "éssim", "éssiu", "essin"),
+            CONDICIONAL to listOf("aria", "aries", "aria", "aríem", "aríeu", "arien"),
+            IMPERATIU to listOf("a", "i", "em", "eu", "in")
+        )
     ),
-    listOf("er", "re") to setOf(
-        "ent",
-        "ut", "uda", "uts", "udes",
-        "o", "s", "", "em", "eu", "en",
-        "ia", "ies", "ia", "iem", "ieu", "ien",
-        "i", "eres", "e", "erem", "ereu", "eren",
-        "ere", "eras", "era", "erem", "ereu", "eran",
-        "i", "is", "i", "em", "eu", "in",
-        "es", "essis", "es", "essim", "essiu", "essin",
-        "eria", "eries", "eria", "eriem", "erieu", "erien",
-        "", "i", "em", "eu", "in"
+    listOf("er", "re") to mapOf(
+        "" to mapOf(
+            GERUNDI to listOf("ent"),
+            PARTICIPI to listOf("ut", "uda", "uts", "udes"),
+            PRESENT to listOf("o", "s", "", "em", "eu", "en"),
+            IMPERFET to listOf("ia", "ies", "ia", "íem", "íeu", "ien"),
+            SIMPLE to listOf("í", "eres", "é", "érem", "éreu", "eren"),
+            FUTUR to listOf("eré", "eràs", "erà", "erem", "ereu", "eran"),
+            SUB_PRESENT to listOf("i", "is", "i", "em", "eu", "in"),
+            SUB_IMPERFET to listOf("és", "essis", "és", "éssim", "éssiu", "essin"),
+            CONDICIONAL to listOf("eria", "eries", "eria", "eríem", "eríeu", "erien"),
+            IMPERATIU to listOf("", "i", "em", "eu", "in")
+        )
     ),
-    listOf("ir") to setOf(
-        "int",
-        "it", "ida", "its", "ides",
-        "o", "s", "", "im", "iu", "en",
-        "eixo", "eixes", "eix", "eixen",
-        "ia", "ies", "ia", "iem", "ieu", "ien",
-        "i", "ires", "i", "irem", "ireu", "iren",
-        "ire", "iras", "ira", "irem", "ireu", "iran",
-        "i", "is", "i", "im", "iu", "in",
-        "eixi", "eixis", "eixi", "eixin",
-        "is", "issis", "is", "issim", "issiu", "issin",
-        "iria", "iries", "iria", "iriem", "irieu", "irien",
-        "", "i", "im", "iu", "in",
-        "eix", "eixi", "eixin"
-    )
+    listOf("ir") to mapOf(
+        "" to mapOf(
+            GERUNDI to listOf("int"),
+            PARTICIPI to listOf("it", "ida", "its", "ides"),
+            PRESENT to listOf("o", "s", "", "im", "iu", "en"),
+            IMPERFET to listOf("ia", "ies", "ia", "íem", "íeu", "ien"),
+            SIMPLE to listOf("í", "ires", "í", "írem", "íreu", "iren"),
+            FUTUR to listOf("iré", "iràs", "irà", "irem", "ireu", "iran"),
+            SUB_PRESENT to listOf("i", "is", "i", "im", "iu", "in"),
+            SUB_IMPERFET to listOf("ís", "issis", "ís", "íssim", "íssiu", "issin"),
+            CONDICIONAL to listOf("iria", "iries", "iria", "iríem", "iríeu", "irien"),
+            IMPERATIU to listOf("", "i", "im", "iu", "in"),
+        ),
+        "incoatius" to mapOf(
+            PRESENT to listOf("eixo", "eixes", "eix", "", "", "eixen"),
+            SUB_PRESENT to listOf("eixi", "eixis", "eixi", "", "", "eixin"),
+            IMPERATIU to listOf("eix", "eixi", "", "", "eixin")
+        ),
+        "ompl" to mapOf(
+            PRESENT to listOf("", "es", "e"),
+            IMPERATIU to listOf("e"),
+        ),
+        "-ompl" to mapOf(
+            PARTICIPI to listOf("", "", "", "", "ert", "erta", "erts", "ertes")
+        )
+    ),
 )
 
 private val IRREGULAR_VERBS = mapOf(
-    "anar" to setOf(
+    "anar" to listOf(
         "vaig", "vas", "va", "van",
         "anire", "aniras", "anira", "anirem", "anireu", "aniran",
         "vagi", "vagis", "vagi", "vagin",
         "aniria", "aniries", "aniria", "aniriem", "anirieu", "anirien",
         "ves", "vagi", "vagin"
     ),
-    "estar" to setOf(
+    "estar" to listOf(
         "estic", "estas", "estan",
         "estigui", "estigueres", "estigue", "estiguerem", "estiguereu", "estigueren",
         "estigui", "estiguis", "estigui", "estiguem", "estigueu", "estiguin",
         "estigues", "estiguessis", "estigues", "estiguessim", "estiguessiu", "estiguessin",
         "estigues", "estigui", "estiguem", "estigueu", "estiguin"
     ),
-    "ser" to setOf(
+    "ser" to listOf(
         "sent", "essent",
         "estat", "estada", "estats", "estades", "sigut", "siguda", "siguts", "sigudes",
         "soc", "ets", "es", "som", "sou", "son",
@@ -75,7 +98,7 @@ private val IRREGULAR_VERBS = mapOf(
         "seria", "series", "seria", "seriem", "serieu", "serien", "fora", "fores", "fora", "forem", "foreu", "foren",
         "sigues", "sigui", "siguem", "sigueu", "siguin"
     ),
-    "fer" to setOf(
+    "fer" to listOf(
         "fet", "feta", "fets", "fetes",
         "faig", "fas", "fa", "fan",
         "feia", "feies", "feia", "feiem", "feieu", "feien",
@@ -86,7 +109,7 @@ private val IRREGULAR_VERBS = mapOf(
         "faria", "faries", "faria", "fariem", "farieu", "farien",
         "fes", "faci", "facin"
     ),
-    "poder" to setOf(
+    "poder" to listOf(
         "pogut", "poguda", "poguts", "pogudes",
         "puc", "pots", "pot",
         "pogui", "pogueres", "pogue", "poguerem", "poguereu", "pogueren",
@@ -96,14 +119,14 @@ private val IRREGULAR_VERBS = mapOf(
         "podria", "podries", "podria", "podriem", "podrieu", "podrien",
         "pugues", "pugui", "puguem", "pugueu", "puguin"
     ),
-    "saber" to setOf(
+    "saber" to listOf(
         "se", "saps", "sap",
         "sabre", "sabras", "sabra", "sabrem", "sabreu", "sabran",
         "sapiga", "sapigues", "sapiga", "sapiguem", "sapigueu", "sapiguen",
         "sabria", "sabries", "sabria", "sabriem", "sabrieu", "sabrien",
         "sapigues", "sapiga", "sapiguem", "sapigueu", "sapiguen"
     ),
-    "tenir" to setOf(
+    "tenir" to listOf(
         "tingut", "tinguda", "tinguts", "tingudes",
         "tinc", "te",
         "tingui", "tingueres", "tingue", "tinguerem", "tinguereu", "tingueren",
@@ -113,7 +136,7 @@ private val IRREGULAR_VERBS = mapOf(
         "tindria", "tindries", "tindria", "tindriem", "tindrieu", "tindrien",
         "ten", "te", "tingues", "tingui", "tinguem", "tingueu", "tinguin"
     ),
-    "haver" to setOf(
+    "haver" to listOf(
         "hagut", "haguda", "haguts", "hagudes",
         "he", "haig", "has", "ha", "hem", "heu", "han",
         "hagui", "hagueres", "hague", "haguerem", "haguereu", "hagueren",
@@ -126,43 +149,89 @@ private val IRREGULAR_VERBS = mapOf(
     )
 )
 
+private val effectiveEndings: Map<List<String>, Map<String, Set<String>>> =
+    VERB_ENDINGS.entries.associate { (infEndings, groups) ->
+        infEndings to groups.entries.associate { (name, group) ->
+            name to groups[""]!!.flatMap { (form, defaultForms) ->
+                val forms = group[form]?.map { it.ifEmpty { null } } ?: listOf()
+                (defaultForms.indices union forms.indices).map { index ->
+                    (forms.getOrNull(index) ?: defaultForms[index]).normalize()
+                }
+            }.toSet()
+        }
+    }
+
+private infix fun IntRange.union(other: IntRange) = IntRange(min(first, other.first), max(last, other.last))
+
+private fun belongsToGroup(base: String, group: String): Boolean {
+    if (group == "" || group == "incoatius") return true //TODO use list of incoatius verbs?
+    return (group.first() == '-' && base.endsWith(group.drop(1))) || group == base
+}
+
 fun infinitivesOf(word: String): List<String> {
     val normalized = word.normalize()
     val irregular = IRREGULAR_VERBS.filter { (_, forms) -> word in forms }
     if (irregular.isNotEmpty()) return irregular.keys.toList()
 
-    val infs = VERB_ENDINGS.flatMap { (infEndings, endings) ->
-        endings
-            .filter { ending -> normalized.endsWith(ending) }
-            .maxByOrNull { ending -> ending.length }
-            ?.let { longestEnding ->
-                infEndings.flatMap { infEnding ->
-                    normalized.replaceEnding(longestEnding, infEnding)
+    val infs = effectiveEndings.flatMap { (infEndings, groups) ->
+        groups.flatMap { (name, group) ->
+            group
+                .filter { ending -> normalized.endsWith(ending) }
+                .maxByOrNull { ending -> ending.length }
+                ?.let { longestEnding ->
+                    val base = word.dropLast(longestEnding.length)
+                    if (!belongsToGroup(base, name)) listOf()
+                    else infEndings.flatMap { infEnding ->
+                        base.replaceEnding(longestEnding, infEnding)
+                    }
                 }
-            }
-            ?.filterNot { inf -> inf in VERB_TYPES }
-            ?: listOf()
+                ?.filterNot { inf -> inf in VERB_TYPES }
+                ?: listOf()
+        }.toSet()
     }
     log.debug("Infinitives of $word: $infs")
     return infs
 }
 
 internal fun String.replaceEnding(oldEnding: String, newEnding: String): List<String> {
-    val base = this.dropLast(oldEnding.length)
-    if (newEnding == "ar") {
-        if (oldEnding.startsWith("i") && base.dropLastWhile { it == 'i' }.last().isVowel())
-            return listOf(base + "iar") //esglais -> esglaiar
-        if (oldEnding.startsWithFront()) return base.frontToBack().map { it + newEnding }
+    val newBase = when (newEnding) {
+        "ar" -> when {
+            oldEnding.startsWith("i") && dropLastWhile { it == 'i' }.last().isVowel() ->
+                listOf(this + "i") //esglais -> esglaiar
+            oldEnding.startsWithFront() -> frontToBack()
+            else -> listOf(this)
+        }
+        "ir" -> when {
+            (endsWith("cull") || endsWith("surt") || endsWith("cup")) &&
+                    oldEnding in listOf("o", "s", "", "i", "en", "is", "in") ->
+                listOfNotNull(
+                    if (oldEnding == "i") this else null,
+                    replaceLast("u", "o")
+                ) //cullo -> collir, culli -> cullir/collir
+            oldEnding.startsWithBack() -> backToFront()
+            else -> listOf(this)
+        }
+        else -> listOf(this)
     }
-    return listOf(base + newEnding)
+    return newBase.map { it + newEnding }
 }
 
 private fun String.frontToBack() = when {
-    endsWith("qu") -> listOf(this, dropLast(2) + "c") //qüe -> quar, que -> car
-    endsWith("c") -> listOf(dropLast(1) + "ç") //ce -> çar
-    endsWith("g") -> listOf(dropLast(1) + "j") //ge -> jar
-    endsWith("gu") -> listOf(this, dropLast(2) + "g") //güe -> guar, gue -> gar
+    endsWith("qu") -> listOf(this, dropLast(2) + "c") //qüe -> qua, que -> ca
+    endsWith("c") -> listOf(dropLast(1) + "ç") //ce -> ça
+    endsWith("g") -> listOf(dropLast(1) + "j") //ge -> ja
+    endsWith("gu") -> listOf(this, dropLast(2) + "g") //güe -> gua, gue -> ga
     else -> listOf(this)
+}
+
+private fun String.backToFront() = when {
+    endsWith("j") -> listOf(dropLast(1) + "g") //jo -> gi
+    else -> listOf(this)
+}
+
+private fun String.replaceLast(find: String, replaceWith: String): String {
+    val pos = lastIndexOf(find)
+    return replaceRange(pos, pos + find.length, replaceWith)
 }
 
 private fun Char.isVowel() = this in "aeiou"
--- src/test/kotlin/guru/nidi/dicamo/GrammarTest.kt
+++ src/test/kotlin/guru/nidi/dicamo/GrammarTest.kt
@@ -8,20 +8,50 @@ class GrammarTest {
     @ParameterizedTest
     @CsvSource(
         textBlock = """
-            parlem,     em, ar, parlar
-            trenques,   es, ar, trenquar/trencar 
-            comences,   es, ar, començar 
-            jugues,     es, ar, juguar/jugar
-            viatges,    es, ar, viatjar         
-            viatges,    es, er, viatger
-            canvis,     is, ar, canvar                     
-            canviis,    is, ar, canviar                     
-            esglais,    is, ar, esglaiar                     
-            esglaiis,   is, ar, esglaiiar                     
+            parl,       em,     ar, parlar
+            trenqu,     es,     ar, trenquar/trencar 
+            comenc,     es,     ar, començar 
+            jugu,       es,     ar, juguar/jugar
+            viatg,      es,     ar, viatjar         
+            viatg,      es,     er, viatger
+            canv,       is,     ar, canvar                     
+            canvi,      is,     ar, canviar                     
+            esgla,      is,     ar, esglaiar                     
+            esglai,     is,     ar, esglaiiar
+            dorm,       im,     ir, dormir
+            fuj,        o,      ir, fugir   
+            cull,       o,      ir, collir   
+            cull,       i,      ir, cullir/collir   
+            surt,       o,      ir, sortir   
+            surt,       i,      ir, surtir/sortir   
+            escup,      o,      ir, escopir   
+            escup,      i,      ir, escupir/escopir
 """
     )
-    fun replaceEnding(word: String, oldEnding: String, newEnding: String, expected: String) {
-        val expectedList = expected.split("/")
-        assertEquals(expectedList, word.replaceEnding(oldEnding, newEnding))
+    fun replaceEnding(base: String, oldEnding: String, newEnding: String, expected: String?) {
+        val expectedList = expected?.split("/") ?: listOf()
+        assertEquals(expectedList, base.replaceEnding(oldEnding, newEnding))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        textBlock = """
+            parlem,     parlar/parler/parlre/parlemir
+            dorme,      dormer/dormre/dormeir
+            dormes,     dormar/dormer/dormre/dormeir     
+            dormert,    dormerter/dormertre/dormertir
+            dormerta,   dormertar/dormertaer/dormertare/dormertair
+            dormerts,   dormerter/dormertre/dormertir
+            dormertes,  dormertar/dormerter/dormertre/dormerteir
+            omple,      ompler/omplre/ompleir/omplir 
+            omplert,    omplerter/omplertre/omplertir/omplir 
+            acomple,    acompler/acomplre/acompleir
+            acompleix,  acompleixer/acompleixre/acompleixir/acomplir
+            acomplert,  acomplerter/acomplertre/acomplertir/acomplir
+"""
+    )
+    fun testInfinitivesOf(word: String, expected: String?) {
+        val expectedList = expected?.split("/") ?: listOf()
+        assertEquals(expectedList, infinitivesOf(word))
     }
 }
